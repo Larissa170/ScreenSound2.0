f50127e984ab9fc30d913bc18572bbef177bbe91
Larissa170 <larissa.m.rodrigues1702@gmail.com>
2024-05-21 20:11:29 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,72 @@
+﻿using ScreenSound.Menus;
+using ScreenSound.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Banda ira = new Banda("Ira");
+        ira.AdicionarNota(new Avaliacao(10));
+        ira.AdicionarNota(new Avaliacao(8));
+        ira.AdicionarNota(new Avaliacao(6));
+        Banda restart = new Banda("Restart");
+        restart.AdicionarNota(new Avaliacao(5));
+        restart.AdicionarNota(new Avaliacao(8));
+        restart.AdicionarNota(new Avaliacao(9));
+
+        Dictionary<string, Banda> bandasRegistradas = new Dictionary<string, Banda>();
+        bandasRegistradas.Add(ira.Nome, ira);
+        bandasRegistradas.Add(restart.Nome, restart);
+
+        Dictionary<int, Menu> opcoes = new();
+        opcoes.Add(1, new MenuRegistrarBanda());
+        opcoes.Add(2,new MenuRegistrarAlbum());
+        opcoes.Add(3, new MenuMostrarbandasRegistradas());
+        opcoes.Add(4, new MenuAvaliarBanda());
+        opcoes.Add(5, new MenuExibirDetalhes());
+        opcoes.Add(-1, new MenuSair());
+
+        void ExibirLogo()
+        {
+            Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+            Console.WriteLine("Boas vindas ao Screen Sound 2.0!");
+        }
+
+        void ExibirOpcoesDoMenu()
+        {
+            ExibirLogo();
+            Console.WriteLine("\nDigite 1 para registrar uma banda");
+            Console.WriteLine("Digite 2 para registrar o álbum de uma banda");
+            Console.WriteLine("Digite 3 para mostrar todas as bandas");
+            Console.WriteLine("Digite 4 para avaliar uma banda");
+            Console.WriteLine("Digite 5 para exibir os detalhes de uma banda");
+            Console.WriteLine("Digite -1 para sair");
+
+            Console.Write("\nDigite a sua opção: ");
+            string opcaoEscolhida = Console.ReadLine()!;
+            int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+            if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
+            {
+                Menu menu = opcoes[opcaoEscolhidaNumerica]; // pega a opcao digitada para indicar qual opcao do dictionary
+                menu.Executar(bandasRegistradas);
+                if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
+
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida");
+            }
+        }
+
+        ExibirOpcoesDoMenu();
+    }
+}
